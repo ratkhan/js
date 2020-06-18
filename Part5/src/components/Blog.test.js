e9ec53501c8238a27f2e3824be728da806bf84cb
Ratkhan Saginbazarov <s.ratkhan@gmail.com>
2020-06-18 01:18:24 -0400

    added new tests

--- Part5/src/components/Blog.test.js
+++ Part5/src/components/Blog.test.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
-import { render } from '@testing-library/react';
 import { prettyDOM } from '@testing-library/dom';
+import { render, fireEvent } from '@testing-library/react';
 import Blog from './Blog';
+import BlogInputForm from "./BlogInputForm";
 
 test (' renders content', () => {
     const blog =
@@ -60,3 +61,49 @@ test (' renders content', () => {
     */
     component.debug();
 });
+
+test('pressing the like button twice, calls the function twice', () => {
+    const blog =
+        {
+            content: {
+                title: 'Blog2',
+                author: 'Ratkhan Saginbazarov',
+                url: 'local',
+                likes: 29
+            },
+            user: {
+                name: 'ratkhan',
+                id: '5ee45042e6b79e258424dd71'
+            },
+            id: '5ee38c5ed0224f43f8a9e05f'
+        };
+
+    const user =
+        {
+            blogs: [],
+            username: 'ratkhan',
+            name: 'ratkhan',
+            id: '5ee45042e6b79e258424dd71'
+        };
+
+    const mockHandler = jest.fn();
+
+    const component = render (
+        <Blog
+            user={user}
+            blog={blog}
+            likeBlog={mockHandler}
+            removeBlog={ () => {} }
+        />
+    );
+
+    const button = component.getByText('Like');
+
+    fireEvent.click(button);
+    fireEvent.click(button);
+    expect(mockHandler.mock.calls).toHaveLength(2);
+})
+
+
+
+
