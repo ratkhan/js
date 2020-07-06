2cc4e4638bbb025b601add2241e67405372ef056
Ratkhan Saginbazarov <s.ratkhan@gmail.com>
2020-07-06 16:19:54 -0400

    added loading and adding new anecdotes into imbedded db (db.json) in redux-anecdotes

--- Part6/redux-anecdotes/src/components/AnecdoteForm.js
+++ Part6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -2,16 +2,18 @@ import React from 'react'
 import {createAnecdote} from "../reducers/anecdoteReducer";
 import { useDispatch } from 'react-redux'
 import {notificationChange} from "../reducers/notificationReducer";
+import anecdoteService from '../services/anecdotes'
 
 const AnecdoteForm = () => {
     const dispatch = useDispatch()
 
-    const addAnecdote = (event) => {
+    const addAnecdote = async (event) => {
         event.preventDefault();
         if (event.target.anecdote.value) {
             const content = event.target.anecdote.value;
             event.target.anecdote.value = '';
-            dispatch(createAnecdote(content));
+            const newAnecdote = await anecdoteService.createNew(content);
+            dispatch(createAnecdote(newAnecdote));
             dispatch(notificationChange("added new anecdote"))
             setTimeout( () => {
                 dispatch(notificationChange(''));
--- Part6/redux-anecdotes/src/components/Notification.js
+++ Part6/redux-anecdotes/src/components/Notification.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { useSelector } from "react-redux";
-import { notificationChange } from "../reducers/notificationReducer";
 
 const Notification = () => {
   const notification = useSelector(state => state.notification);
